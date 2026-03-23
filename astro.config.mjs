8696885d39e3d562e6a3809b15186e5ead5a12c6
alpermumcu <alper@mumcu.com>
2026-03-23 21:59:10 +0300

    Fix build by removing problematic sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://mumcu.com',
-  integrations: [mdx(), sitemap()],
+  site: 'https://mumcu.com/',
+  integrations: [mdx()],
 });
