cc272715cff6ec22467e4afc47cbe3b194282f34
alpermumcu <alper@mumcu.com>
2026-03-27 12:55:09 +0300

    chore: trigger fresh build to restore stable Astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://mumcu.com',
+  // Trigger build to restore stable version
   integrations: [sitemap()],
 });
+
